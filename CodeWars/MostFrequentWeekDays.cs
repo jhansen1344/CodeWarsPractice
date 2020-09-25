49bdeaad568f6ccd8b9517a8605e0c03426e3b71
Jeremy Hansen <jhansen1344@gmail.com>
2020-09-25 15:09:56 -0400

    mostfrequentdays updated

---
+++ CodeWars/MostFrequentWeekDays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace CodeWars
+{
+    public class Kata
+    {
+        public static string[] MostFrequentDays(int year)
+        {
+            var startDate = new DateTime(year, 1, 1);
+            var endDate = new DateTime(year, 12, 31);
+
+            var firstDay = startDate.DayOfWeek;
+            var arrayLength = 1;
+            var returnArray = new string[1] { firstDay.ToString() };
+            while (endDate.DayOfWeek.ToString() != firstDay.ToString())
+            {
+                arrayLength += 1;
+                Array.Resize<string>(ref returnArray, arrayLength);
+                returnArray[arrayLength - 1] = endDate.DayOfWeek.ToString();
+                endDate = endDate.AddDays(-1);
+            };
+            return returnArray;
+        }
+    }
+}
---
+++ Kata Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using CodeWars;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MostFrequentDaysTests()
+        {
+                Assert.AreEqual(new[] { "Friday", "Saturday" }, Kata.MostFrequentDays(2016));
+                Assert.AreEqual(new[] { "Monday" }, Kata.MostFrequentDays(1770));
+                Assert.AreEqual(new[] { "Monday" }, Kata.MostFrequentDays(2001));
+                Assert.AreEqual(new[] { "Monday", "Tuesday" }, Kata.MostFrequentDays(1968));
+                Assert.AreEqual(new[] { "Saturday" }, Kata.MostFrequentDays(1785));
+                Assert.AreEqual(new[] { "Saturday" }, Kata.MostFrequentDays(1910));
+                Assert.AreEqual(new[] { "Saturday" }, Kata.MostFrequentDays(2135));
+                Assert.AreEqual(new[] { "Sunday" }, Kata.MostFrequentDays(3043));
+                Assert.AreEqual(new[] { "Sunday" }, Kata.MostFrequentDays(3150));
+                Assert.AreEqual(new[] { "Thursday" }, Kata.MostFrequentDays(3361));
+                Assert.AreEqual(new[] { "Tuesday" }, Kata.MostFrequentDays(1901));
+                Assert.AreEqual(new[] { "Tuesday" }, Kata.MostFrequentDays(3230));
+                Assert.AreEqual(new[] { "Wednesday" }, Kata.MostFrequentDays(1794));
+                Assert.AreEqual(new[] { "Wednesday" }, Kata.MostFrequentDays(1986));
+        }
+    }
+    
+}
