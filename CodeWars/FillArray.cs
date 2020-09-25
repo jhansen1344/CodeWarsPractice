cc0d6c880c09f9dd533ed82b29d49f892de3cc06
Jeremy Hansen <jhansen1344@gmail.com>
2020-09-25 15:37:03 -0400

    fill array completed

---
+++ CodeWars/FillArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class FillArray
+    {
+        public static int[] Arr(int N)
+        {
+
+            var returnArray = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+
+                returnArray[i] = i;
+            }
+            return returnArray;
+        }
+    }
+}
