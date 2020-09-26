b30d1efa1e80dd7da8aae6277e55d5471019e095
Jeremy Hansen <jhansen1344@gmail.com>
2020-09-26 17:04:12 -0400

    String Doubles Added

---
+++ CodeWars/StringDoubles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class StringDoubles
+    {
+        public static string Doubles(string str)
+        {
+            char[] stringSplit = str.ToCharArray();
+            string returnString = "";
+            for (int i = 0; i < stringSplit.Length - 1; i++)
+            {
+                if (stringSplit[i] != stringSplit[i + 1])
+                {
+                    returnString += stringSplit[i];
+                }
+                else
+                {
+                    i++;
+                }
+
+            }
+            if (stringSplit[stringSplit.Length - 1] != stringSplit[stringSplit.Length - 2])
+            {
+                returnString += stringSplit[stringSplit.Length - 1];
+            }
+            return returnString;
+        }
+    }
+}
