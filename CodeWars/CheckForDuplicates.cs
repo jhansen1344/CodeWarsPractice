65adafa0081673e1c38fd6425ce200c54d06c7db
Jeremy Hansen <jhansen1344@gmail.com>
2020-11-04 21:53:56 -0500

    check for duplicates in array

---
+++ CodeWars/CheckForDuplicates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class CheckForDuplicates
+    {
+
+        public static bool CheckForMultiple(int[] inputArray)
+        {
+            var inputHashSet = new HashSet<int>();
+            foreach (var item in inputArray)
+            {
+                if(inputHashSet.Contains(item))
+                {
+                    return true;
+                }
+                inputHashSet.Add(item);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ CodeWars/PyramidArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class PyramidArray
+    {
+
+        public static int[][] Pyramid(int n)
+        {
+            // your code here
+            if (n == 0)
+                return new int[][] { };
+            var returnArray = new int[][,] { };
+            var testHashSet = new HashSet<int>();
+
+           var inputString = "test";
+            var inputLength = inputString.Length;
+            var stringArray = inputString.ToCharArray();
+            var charDictionary = new Dictionary<char, int>();
+            foreach (var item in stringArray)
+            {
+                if(!charDictionary.ContainsKey(item))
+                {
+                    charDictionary.Add(item, 1);
+                }
+                else
+                {
+                    charDictionary[item] ++;
+                }
+            }
+            var maxOdd = 0;
+            foreach (var item in charDictionary)
+            {
+                if (item.Value % 2 == 1)
+                    maxOdd++;
+
+                if (maxOdd > 1)
+                    return false;
+
+            }
+            return true;
+
+
+
+                return returnArray;
+        }
+        
+	{
+            
+            
+	}
+    }
+
+
+}
